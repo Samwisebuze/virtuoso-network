ea98d7599e628d0976946f0968c0f5dc4acca4f9
Samuel Buzas <buzassg@mail.uc.edu>
2020-03-07 14:26:01 -0500

    initial commit

---
+++ src/api/networks.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+
+const router = express.Router();
+
+// Service Root Path - /machines
+// HATEOAS structure to discover the rest of the service.
+router.get('', async (req, res, next) => {
+    const { networkId, machineId } = req.body;
+    
+    res.status(501).json();
+});
+
+router.post('', async (req, res, next) => {
+    res.status(501).json();
+});
+
+router.get('/:networkId', async (req, res, next) => {
+    res.status(501).json();
+});
+
+router.patch('/:networkId', async (req, res, next) => {
+    res.status(501).json();
+});
+
+router.delete('/:networkId', async (req, res, next) => {
+    res.status(501).json();
+});
+
+export default router;
